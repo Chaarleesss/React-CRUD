7369ceb4fa6f20c89b58fdb5a5b6544c8b586178
CharlesAndriansyah <95111487+clean10@users.noreply.github.com>
2022-08-03 17:06:33 +0700

    upload 1

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkmode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
